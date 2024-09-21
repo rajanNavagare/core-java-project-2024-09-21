bb3604076aa41899150dd502583d198b6e5f918c
rajanv2 <rajan.navagare@v2solutions.com>
2024-09-21 16:38:24 +0530

    first commit

---
+++ src/main/java/two_thousand_twenty_four/september/twenty_first/TestClass1.java
@@ -0,0 +1,14 @@
+package two_thousand_twenty_four.september.twenty_first;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestClass1 {
+
+    public static void main(String[] args) {
+        System.out.println("TestClass1.main()");
+        log.info("test class");
+        
+    }
+
+}
