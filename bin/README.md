f6ca666bdf578eee98d00fcea9a4303f2cddf44d
rajanNavagare <rajan.v.navagare@gmail.com>
2024-09-21 17:10:32 +0530

    2nd commit

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# core-java-projects
+core java code
--- src/main/java/two_thousand_twenty_four/september/twenty_first/TestClass1.java
+++ src/main/java/two_thousand_twenty_four/september/twenty_first/TestClass1.java
@@ -8,7 +8,6 @@ public class TestClass1 {
     public static void main(String[] args) {
         System.out.println("TestClass1.main()");
         log.info("test class");
-        
     }
 
 }
